0a8b3057d9e72d7855d8d9c10223a6ddc09432ac
Kone-nantenin <151059407+Kone-nantenin@users.noreply.github.com>
2023-11-17 14:58:12 +0100

    Add files via upload

---
+++ Yamoussokro.sql
@@ -0,0 +1,93 @@
+!DOCTYPE html>
+ 
+<html lang="fr">
+ 
+<head>
+ 
+    <meta charset="UTF-8">
+ 
+    <meta name="author" content=" kone nantenin ">
+ 
+    <meta name="description" content="Cette page vous fait découvrir la capitale de cote d'ivoire :yamoussokro">
+ 
+    <title>yamoussokro</title>
+ 
+</head>
+ 
+<body style="background-color: lightgreen;">
+<h1>Rome, capitale cote d'ivoire</h1>
+ 
+<img src=https://discover-ivorycoast.com/wp-content/uploads/2019/01/basilique-Notre-Dame-de-la-Paix-de-Yamoussoukro-2.jpg
+<p>yamoussokro est la capitale de l'<a href="https://cdn.britannica.com/00/182000-050-FBC509C0/Abidjan-Cote-Ivoire.jpg
+ 
+<p style="color: yellow;">Lorem ipsum</p>
+ 
+<img src="https://viajes.nationalgeographic.com.es/medio/2021/05/12/roma_3bef6ec9_1254x836.jpg" width="40%" height="40%">
+<div>
+ 
+    <p>Liste de quelques villes de cote d'ivoire :</p>
+ 
+    <ul>
+ 
+        <li><a href="https://it.readkong.com/static/1c/bf/1cbfe4558be6562c633bf153cf41ff50/cdp-iclei-il-sistema-unificato-di-reporting-regione-4619660-10.jpg
+ 
+        <li>Abidjan</li>
+ 
+        <li>Bouaké</li>
+ 
+        <li>Vavoua</li>
+ 
+    </ul>
+<table border="1" style="width:100%;">
+ 
+        <caption>Liste des personnes</caption>
+ 
+        <tr>
+ 
+            <th>ID</th>
+ 
+            <th>Nom</th>
+ 
+            <th>Prénom</th>
+ 
+        </tr>
+ 
+        <tr>
+ 
+            <td>01</td>
+ 
+            <td>Giovanni</td>
+ 
+            <td>Luigi</td>
+ 
+        </tr>
+ 
+        <tr>
+ 
+            <td>02</td>
+ 
+            <td>Sofia</td>
+ 
+            <td>Maria</td>
+ 
+        </tr>
+ 
+        <tr>
+ 
+            <td>03</td>
+ 
+            <td>Alessio</td>
+ 
+            <td>Francesca</td>
+ 
+        </tr>
+ 
+    </table>
+ 
+</div>
+</body>
+ 
+</html>
+
+Dispose d’un menu contextuel
+Composer
